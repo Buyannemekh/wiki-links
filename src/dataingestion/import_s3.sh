e31363c0acdca9f87afcc05d8a00d5b6e1034a4c
buyannemekh <munkh22b@mtholyoke.edu>
2019-06-23 13:03:20 -0400

    bash add do

--- src/dataingestion/import_s3.sh
+++ src/dataingestion/import_s3.sh
@@ -1,5 +1,5 @@
-while read p;
+while read p; do
     export NAME = ${p/https://dumps.wikimedia.org/enwiki/latest/enwiki-latest-/}
     echo $NAME
     ##do  curl "$p" | aws s3 cp - s3://wikipedia-article-sample-data/"$NAME";
-    done < text_http.txt
+done < text_http.txt
