fe4dafeacd9f845d975efd1d5e69a69dd7c70b91
buyannemekh <munkh22b@mtholyoke.edu>
2019-06-22 21:37:18 -0400

    test http links

--- src/dataingestion/download_links.sh
+++ src/dataingestion/download_links.sh
@@ -15,3 +15,6 @@ while read line; do echo "https://dumps.wikimedia.org/enwiki/latest/$line"; done
 
 # single file download
 # curl https://dumps.wikimedia.org/enwiki/latest/enwiki-latest-pages-meta-history10.xml-p3040817p3046511.bz2  | aws s3 cp - s3://wiki-meta/tt10_t
+
+# all file download
+
