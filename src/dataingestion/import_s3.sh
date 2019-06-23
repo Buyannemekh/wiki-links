2a38a8d80913a14e6fef8e2a78f35a07452fadf1
buyannemekh <munkh22b@mtholyoke.edu>
2019-06-23 13:05:50 -0400

    backslash str

--- src/dataingestion/import_s3.sh
+++ src/dataingestion/import_s3.sh
@@ -1,5 +1,5 @@
 while read p; do
-    export NAME = ${p/https://dumps.wikimedia.org/enwiki/latest/enwiki-latest-/}
+    export NAME = ${p/https:\/\/dumps.wikimedia.org\/enwiki\/latest\/enwiki-latest-/}
     echo $NAME
     ##do  curl "$p" | aws s3 cp - s3://wikipedia-article-sample-data/"$NAME";
 done < text_http.txt
