fbec0ce816b8004eda0168265bd049dd29a1eece
buyannemekh <munkh22b@mtholyoke.edu>
2019-06-24 11:28:17 -0400

    timestamp formate in the example

--- src/postgres/postgres.py
+++ src/postgres/postgres.py
@@ -7,7 +7,7 @@ connection = psycopg2.connect(host='ec2-34-239-95-229.compute-1.amazonaws.com',
 cursor = connection.cursor()
 
 cursor.execute("INSERT INTO links (article_id, link_name, first_time_stamp, deleted_time_stamp) " +
-               "VALUES " + "(0, 'Hello World', 2006-11-09T06:43:10Z, 2018-06-19T05:56:36Z)")
+               "VALUES " + "(0, 'Hello World', 2006-11-09, 2018-06-19)")
 
 connection.commit()
 
