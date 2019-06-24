436cbc78b44ce22225b8d7f14507e64ba870f66e
buyannemekh <munkh22b@mtholyoke.edu>
2019-06-24 11:51:36 -0400

    timestamp sql test

--- src/postgres/postgres.py
+++ src/postgres/postgres.py
@@ -7,7 +7,7 @@ connection = psycopg2.connect(host='ec2-34-239-95-229.compute-1.amazonaws.com',
 cursor = connection.cursor()
 
 cursor.execute("INSERT INTO links (article_id, link_name, first_time_stamp, deleted_time_stamp) " +
-               "VALUES " + "(0, 'Hello World', 2006-11-09, 2018-06-19)")
+               "VALUES " + "(0, 'Hello World', '2010-08-25 01:11:11', '2017-04-30 13:37:40')")
 
 connection.commit()
 
