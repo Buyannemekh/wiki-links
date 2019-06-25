33b246cf6d9fd16a7b82ba562732153082f5d241
buyannemekh <munkh22b@mtholyoke.edu>
2019-06-25 08:07:16 -0400

    added comments

--- src/spark/parse_xml.py
+++ src/spark/parse_xml.py
@@ -18,32 +18,59 @@ class ParseXML:
         self.page_df_text = self.get_page_df_from_xml()  # data frame with text
         self.page_df_links = self.create_df_of_links()   # data frame with links
         self.page_df_id_link_time = self.explode_links()   # data frame with exploded links
-        self.link_df_min_timestamp = self.group_by_id_link()
+        self.link_df_min_timestamp = self.group_by_id_link()  # find the earliest timestamp for a link in an article
 
     # parse xml and extract information under revision tag
     def get_page_df_from_xml(self):
-        page_df = self.spark.read.format(self.format).options(rowTag=self.row_tag_title).load(self.file).persist()
-        df = page_df.select(f.col('id'), f.col('revision.text'), f.col('revision.timestamp'))
-        df = df.withColumn("time", df.timestamp.cast(TimestampType()))
-        return df
+
+        page_df = self.spark.read\
+            .format(self.format)\
+            .options(rowTag=self.row_tag_title)\
+            .load(self.file)\
+            .persist()
+
+        # create df with article id, text, and revision timestamp
+        df_id_text_time = page_df.select(f.col('id'),
+                                         f.col('revision.text'),
+                                         f.col('revision.timestamp'))
+
+        # cast timestamp as timestamp type for future query
+        df_id_text_time = df_id_text_time.withColumn("time", df_id_text_time.timestamp.cast(TimestampType()))
+
+        return df_id_text_time
 
     # extract links from the text and create data frame with list of link titles
     def create_df_of_links(self):
+
         find_links_udf = udf(find_links, ArrayType(StringType()))
-        df = self.page_df_text.withColumn('links', find_links_udf(self.page_df_text.text))
-        df_links = df.select(f.col('id'), f.col('time'), f.col('links'))
+
+        # find links from the text column using regex with udf from df with text column
+        df = self.page_df_text.withColumn('links',
+                                          find_links_udf(self.page_df_text.text))
+
+        # dataframe with article id, revision timestamp, array of links in the text
+        df_links = df.select(f.col('id'),
+                             f.col('time'),
+                             f.col('links'))
+
         return df_links
 
+    # create column with a single link
     def explode_links(self):
-        df = self.page_df_links.withColumn("link", explode(self.page_df_links.links))
-        df_id_link_time = df.select(f.col('id'), f.col('time'), f.col('link'))
-        return df_id_link_time
+        # create column of single link name
+        df_id_link_time = self.page_df_links.withColumn("link", explode(self.page_df_links.links))
 
+        # create dataframe with article id, revision timestamp, link name (dropping links)
+        page_df_id_link_time = df_id_link_time.select(f.col('id'),
+                                                      f.col('time'),
+                                                      f.col('link'))
+
+        return page_df_id_link_time
+
+    # when multiple revisions, find the earliest creation date for a link in an article
     def group_by_id_link(self):
-        df_count = self.page_df_id_link_time.groupby("id", "link").count().sort(desc("count"))
-        df_count.show()
-        df = self.page_df_id_link_time.groupby("id", "link").agg(f.min("time"))
-        return df
+        df_earliest_timestamp = self.page_df_id_link_time.groupby("id", "link").agg(f.min("time"))
+        return df_earliest_timestamp
 
 
 # return list of link titles from a text if exist, else return empty list
@@ -56,15 +83,22 @@ def find_links(text):
         return []
 
 
+# helper for printing dataframe number of rows and columns
+def print_df_count(df):
+    df.printSchema()
+    print(df.count(), len(df.columns))
+    df.show()
+
+
 if __name__ == "__main__":
     input_file = "s3a://wikipedia-article-sample-data/enwiki-latest-pages-articles14.xml-p7697599p7744799.bz2"
     process = ParseXML(input_file)
-    process.page_df_text.printSchema()
-    process.page_df_text.show()
-    process.page_df_links.where(process.page_df_links.links.isNotNull()).show()
-    process.page_df_links.select(f.col("links")).show()
-    print(process.page_df_links.count(), len(process.page_df_links.columns))
-    process.page_df_id_link_time.show()
-    print(process.page_df_id_link_time.count(), len(process.page_df_id_link_time.columns))
-    process.link_df_min_timestamp.show()
-    print(process.link_df_min_timestamp.count(), len(process.link_df_min_timestamp.columns))
+    print_df_count(process.page_df_text)
+
+    print_df_count(process.page_df_links)
+    print_df_count(process.page_df_id_link_time)
+
+    df_count = process.page_df_id_link_time.groupby("id", "link").count().sort(desc("count"))
+    df_count.show()
+
+    print_df_count(process.link_df_min_timestamp)
