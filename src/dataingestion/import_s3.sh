78668a5025c04c367e54717125744b2883328879
buyannemekh <munkh22b@mtholyoke.edu>
2019-06-26 17:16:04 -0400

    part 3

--- src/dataingestion/import_s3.sh
+++ src/dataingestion/import_s3.sh
@@ -1,5 +1,5 @@
 while read p; do
     export NAME=${p/https:\/\/dumps.wikimedia.org\/enwiki\/latest\/enwiki-latest-pages-meta-/}
     # echo $NAME
-    curl "$p" | aws s3 cp - s3://wiki-current-part2/"$NAME";
-done < http_bz2_current_links_part2.txt
+    curl "$p" | aws s3 cp - s3://wiki-current-part3/"$NAME";
+done < http_bz2_current_links_part3.txt
