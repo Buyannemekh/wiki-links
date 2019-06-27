02f235ff1887139c1cf4de46628c91e02802c947
buyannemekh <munkh22b@mtholyoke.edu>
2019-06-27 15:56:33 -0400

    connect postgres

--- src/dash/app.py
+++ src/dash/app.py
@@ -1,13 +1,45 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from datetime import datetime as dt
+import psycopg2
+import pandas as pd
+
+
+
+# Settings for psycopg Timescale connector
+user = 'postgres'
+host = 'ec2-34-239-95-229.compute-1.amazonaws.com'
+dbname = 'wiki'
+con = psycopg2.connect(database=dbname, user=user, password='$password', host='localhost')
+
+
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
+
+# Query database to load landing page graph
+sql_query_0 = "SELECT link, COUNT(*) FROM pages_links" + \
+              "WHERE time_stamp BETWEEN '2019-06-01' AND CURRENT_TIMESTAMP " + \
+              "GROUP BY link ORDER BY COUNT(*) DESC LIMIT 20"
+
+query_results_0 = pd.read_sql_query(sql_query_0, con)
+print(query_results_0)
+revs_0 = []
+# for i in range(0, query_results_0.shape[0]):
+#     revs_0.append(dict(time=query_results_0.iloc[i]['time'], frequency=query_results_0.iloc[i]['frequency']))
+
+
+for i in range(0,query_results_0.shape[0]):
+    revs_0.append(dict(time=query_results_0.iloc[i]['time'], frequency=query_results_0.iloc[i]['frequency']))
+
 app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
+    html.H1(children='Wiki Links',
+            style={'textAlign': 'center',
+                   'margin-top': '80px',
+                   'margin-bottom': '80px'
+                   }),
 
     html.Div(children='''
         Dash: A web application framework for Python.
