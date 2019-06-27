664a3d7563cf55b30c3ef190ab7ed72f3da7e613
buyannemekh <munkh22b@mtholyoke.edu>
2019-06-27 13:12:31 -0400

    medium file

--- src/spark/parse_xml.py
+++ src/spark/parse_xml.py
@@ -99,7 +99,7 @@ class ParseXML:
         return page_df_id_link_time
 
     def count_num_each_link_in_page(self):
-        df = self.page_df_id_link_time.groupby("page_id", "page_title", "revision_id", "link_name", "time").\
+        df = self.page_df_id_link_time.groupby("page_id", "page_title", "revision_id", "link", "time").\
             agg(f.count(f.lit(1)).alias("link_count"))
         return df
 
