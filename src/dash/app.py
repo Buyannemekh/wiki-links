c1f3e7f9e505754eb4e00b51ee5a204cd81f508f
buyannemekh <munkh22b@mtholyoke.edu>
2019-06-27 16:16:13 -0400

    first bar plot

--- src/dash/app.py
+++ src/dash/app.py
@@ -2,60 +2,33 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from datetime import datetime as dt
-# import psycopg2
-# import pandas as pd
-#
-#
-#
-# # Settings for psycopg Timescale connector
-# user = 'postgres'
-# host = 'ec2-34-239-95-229.compute-1.amazonaws.com'
-# dbname = 'wiki'
-# con = psycopg2.connect(database=dbname, user=user, password='$password', host=host)
-#
-#
-#
-# # Query database to load landing page graph
-# sql_query_0 = "SELECT link, COUNT(*) FROM pages_links " + \
-#               "WHERE time_stamp BETWEEN '2019-06-01' AND CURRENT_TIMESTAMP " + \
-#               "GROUP BY link ORDER BY COUNT(*) DESC LIMIT 20"
-#
-# query_results_0 = pd.read_sql_query(sql_query_0, con)
-# links = []
-# for i in range(0, query_results_0.shape[0]):
-#     links.append(dict(time=query_results_0.iloc[i]['link'], frequency=query_results_0.iloc[i]['count']))
-#
-# print()
+import psycopg2
+import pandas as pd
+
+
+
+# Settings for psycopg Timescale connector
+user = 'postgres'
+host = 'ec2-34-239-95-229.compute-1.amazonaws.com'
+dbname = 'wiki'
+con = psycopg2.connect(database=dbname, user=user, password='$password', host=host)
+
+
+
+# Query database to load landing page graph
+sql_query_0 = "SELECT link, COUNT(*) FROM pages_links " + \
+              "WHERE time_stamp BETWEEN '2019-06-01' AND CURRENT_TIMESTAMP " + \
+              "GROUP BY link ORDER BY COUNT(*) DESC LIMIT 20"
+
+query_results_0 = pd.read_sql_query(sql_query_0, con)
+links = []
+for i in range(0, query_results_0.shape[0]):
+    links.append(dict(time=query_results_0.iloc[i]['link'], frequency=query_results_0.iloc[i]['count']))
+
+print()
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
-app.layout = html.Div(children=[
-    html.H1(children='Wiki Links',
-            style={'textAlign': 'center',
-                   'margin-top': '80px',
-                   'margin-bottom': '80px'
-                   }),
-
-    html.Div(children='''
-        Dash: A web application framework for Python.
-    '''),
-
-    dcc.Graph(
-        id='example-graph',
-        figure={
-            'data': [
-                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': 'Montréal'},
-            ],
-            'layout': {
-                'title': 'Dash Data Visualization'
-            }
-        }
-    )
-])
-
-
 
 # app.layout = html.Div(children=[
 #     html.H1(children='Wiki Links',
@@ -72,14 +45,40 @@ app.layout = html.Div(children=[
 #         id='example-graph',
 #         figure={
 #             'data': [
-#                 {'x': query_results_0['link'], 'y': query_results_0['count'], 'type': 'bar', 'name': 'Links'}
+#                 {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
+#                 {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': 'Montréal'},
 #             ],
 #             'layout': {
-#                 'title': 'The most cited Wikipedia pages in the past month'
+#                 'title': 'Dash Data Visualization'
 #             }
 #         }
 #     )
 # ])
+
+
+app.layout = html.Div(children=[
+    html.H1(children='Wiki Links',
+            style={'textAlign': 'center',
+                   'margin-top': '80px',
+                   'margin-bottom': '80px'
+                   }),
+
+    html.Div(children='''
+        Dash: A web application framework for Python.
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure={
+            'data': [
+                {'x': query_results_0['link'], 'y': query_results_0['count'], 'type': 'bar', 'name': 'Links'}
+            ],
+            'layout': {
+                'title': 'The most cited Wikipedia pages in the past month'
+            }
+        }
+    )
+])
 
 if __name__ == '__main__':
     app.run_server(debug=True)
