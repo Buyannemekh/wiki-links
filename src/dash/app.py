5d5f7c600b85e76a62e774e381becda61f1fc8ac
buyannemekh <munkh22b@mtholyoke.edu>
2019-06-27 16:13:33 -0400

    first bar plot

--- src/dash/app.py
+++ src/dash/app.py
@@ -2,34 +2,33 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from datetime import datetime as dt
-import psycopg2
-import pandas as pd
-
-
-
-# Settings for psycopg Timescale connector
-user = 'postgres'
-host = 'ec2-34-239-95-229.compute-1.amazonaws.com'
-dbname = 'wiki'
-con = psycopg2.connect(database=dbname, user=user, password='$password', host=host)
-
-
+# import psycopg2
+# import pandas as pd
+#
+#
+#
+# # Settings for psycopg Timescale connector
+# user = 'postgres'
+# host = 'ec2-34-239-95-229.compute-1.amazonaws.com'
+# dbname = 'wiki'
+# con = psycopg2.connect(database=dbname, user=user, password='$password', host=host)
+#
+#
+#
+# # Query database to load landing page graph
+# sql_query_0 = "SELECT link, COUNT(*) FROM pages_links " + \
+#               "WHERE time_stamp BETWEEN '2019-06-01' AND CURRENT_TIMESTAMP " + \
+#               "GROUP BY link ORDER BY COUNT(*) DESC LIMIT 20"
+#
+# query_results_0 = pd.read_sql_query(sql_query_0, con)
+# links = []
+# for i in range(0, query_results_0.shape[0]):
+#     links.append(dict(time=query_results_0.iloc[i]['link'], frequency=query_results_0.iloc[i]['count']))
+#
+# print()
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
-
-# Query database to load landing page graph
-sql_query_0 = "SELECT link, COUNT(*) FROM pages_links " + \
-              "WHERE time_stamp BETWEEN '2019-06-01' AND CURRENT_TIMESTAMP " + \
-              "GROUP BY link ORDER BY COUNT(*) DESC LIMIT 20"
-
-query_results_0 = pd.read_sql_query(sql_query_0, con)
-links = []
-for i in range(0, query_results_0.shape[0]):
-    links.append(dict(time=query_results_0.iloc[i]['link'], frequency=query_results_0.iloc[i]['count']))
-
-print()
 
 app.layout = html.Div(children=[
     html.H1(children='Wiki Links',
