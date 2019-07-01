618d2f988cfa5a6355c560cd7719af8533f83b53
buyannemekh <munkh22b@mtholyoke.edu>
2019-07-01 18:16:54 -0400

    batch

--- src/batch_process/spark.py
+++ src/batch_process/spark.py
@@ -5,7 +5,7 @@ import os
 import sys
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/src")
-from postgres.write_to_postgres import PostgresConnector
+from src.batch_process.postgres import *
 
 
 class ParseXML:
