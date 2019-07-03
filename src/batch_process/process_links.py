53374c0274c88203e8c0e509f1957187b0b67ae3
buyannemekh <munkh22b@mtholyoke.edu>
2019-07-02 23:40:39 -0400

    read from psql

--- src/batch_process/process_links.py
+++ src/batch_process/process_links.py
@@ -16,12 +16,24 @@ properties = {
     "driver": "org.postgresql.Driver"
 }
 
+user = os.environ["POSTGRES_USER"]
+password = os.environ["POSTGRES_PASSWORD"]
 hostname = os.environ["POSTGRES_HOSTNAME"]
 database = os.environ["POSTGRES_DBNAME"]
 port = "5432"
 url = "jdbc:postgresql://{0}:{1}/{2}".format(hostname, port, database)
 
+tx_df = spark.read\
+    .format("jdbc") \
+    .option("url", url) \
+    .option("user", user) \
+    .option("password", password) \
+    .option("dbtable", "links") \
+    .option("numPartitions", '10000') \
+    .load()
+# display_df(tx_df)
 
-jdbcDF2 = spark.read.jdbc(url=url, table="links", properties=properties)
-
+#jdbcDF2 = spark.read.jdbc(url=url, table="links", properties=properties)
+tx_df.show()
+print(tx_df.count(), len(tx_df.columns))
 print("Postgres to Spark done")
