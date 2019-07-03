e56407a69b1b29100c3fce8cace5b50cabdee328
buyannemekh <munkh22b@mtholyoke.edu>
2019-07-02 23:13:03 -0400

    links

---
+++ src/batch_process/process_links.py
@@ -0,0 +1,27 @@
+from pyspark.sql import SparkSession
+import sys
+import os
+
+
+spark = SparkSession.builder.getOrCreate()
+
+os.environ["POSTGRES_HOSTNAME"] = sys.argv[1]
+os.environ["POSTGRES_USER"] = sys.argv[2]
+os.environ["POSTGRES_PASSWORD"] = sys.argv[3]
+os.environ["POSTGRES_DBNAME"] = sys.argv[4]
+
+properties = {
+    "user": os.environ["POSTGRES_USER"],
+    "password": os.environ["POSTGRES_PASSWORD"],
+    "driver": "org.postgresql.Driver"
+}
+
+hostname = os.environ["POSTGRES_HOSTNAME"]
+database = os.environ["POSTGRES_DBNAME"]
+port = "5432"
+url = "jdbc:postgresql://{0}:{1}/{2}".format(hostname, port, database)
+
+
+jdbcDF2 = spark.read.jdbc(url=url, table="links", properties=properties)
+
+print("Postgres to Spark done")
