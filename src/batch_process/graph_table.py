d0be4618fbc030410a7651c05d688f2d9958fd5d
buyannemekh <munkh22b@mtholyoke.edu>
2019-07-07 19:59:22 -0400

    read pages from postgres

---
+++ src/batch_process/graph_table.py
@@ -0,0 +1,38 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import count
+import sys
+import os
+
+
+spark = SparkSession.builder.getOrCreate()
+
+os.environ["POSTGRES_HOSTNAME"] = sys.argv[1]
+os.environ["POSTGRES_USER"] = sys.argv[2]
+os.environ["POSTGRES_PASSWORD"] = sys.argv[3]
+os.environ["POSTGRES_DBNAME"] = sys.argv[4]
+
+properties = {
+    "user": os.environ["POSTGRES_USER"],
+    "password": os.environ["POSTGRES_PASSWORD"],
+    "driver": "org.postgresql.Driver"
+}
+
+user = os.environ["POSTGRES_USER"]
+password = os.environ["POSTGRES_PASSWORD"]
+hostname = os.environ["POSTGRES_HOSTNAME"]
+database = os.environ["POSTGRES_DBNAME"]
+port = "5432"
+url = "jdbc:postgresql://{0}:{1}/{2}".format(hostname, port, database)
+
+
+tx_df = spark.read\
+    .format("jdbc") \
+    .option("url", url) \
+    .option("user", user) \
+    .option("password", password) \
+    .option("dbtable", "pages") \
+    .load()
+
+tx_df.show(20)
+print(tx_df.printSchema())
+print(tx_df.count(), len(tx_df.columns))
