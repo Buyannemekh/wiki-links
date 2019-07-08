197c85327d040de13f51a18d521b0f68ea3752b4
Ubuntu <ubuntu@ip-10-0-0-105.ec2.internal>
2019-07-08 21:50:56 +0000

    table

--- src/batch_process/graph_table.py
+++ src/batch_process/graph_table.py
@@ -1,5 +1,6 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import count
+from pyspark.sql.functions import col, explode
 import sys
 import os
 
@@ -34,5 +35,35 @@ tx_df = spark.read\
     .load()
 
 tx_df.show(20)
-print(tx_df.printSchema())
-print(tx_df.count(), len(tx_df.columns))
+sorted_table = tx_df.orderBy("page_id", ascending=False)
+sorted_table.show(20)
+
+tx_df_distinct = sorted_table.dropDuplicates(['page_id'])
+
+
+look_up_df = tx_df_distinct.select(col('page_id').alias('link_id'),
+                           col('page_title'))
+look_up_df.show(20)
+print(look_up_df.printSchema())
+print(look_up_df.count(), len(look_up_df.columns))
+
+pages_links_df = tx_df_distinct.withColumn("link", explode(tx_df.links)).select(col('page_id'), col('link'))
+print(pages_links_df.printSchema())
+print(pages_links_df.count(), len(pages_links_df.columns))
+pages_links_df.show(20)
+
+
+end_table = look_up_df.join(pages_links_df, look_up_df.page_title == pages_links_df.link).select('link_id', 'page_title', 'page_id')
+print(end_table.printSchema())
+print(end_table.count(), len(end_table.columns))
+end_table.show(20)
+
+#orderby_link_id_df = end_table.orderBy("link_id")
+#print(orderby_link_id_df.printSchema())
+#print(orderby_link_id_df.count(), len(orderby_link_id_df.columns))
+#orderby_link_id_df.show(20)
+
+popularity_df = end_table.groupBy('link_id').agg(count('*').alias('cite_count'))
+print(popularity_df.printSchema())
+print(popularity_df.count(), len(popularity_df.columns))
+popularity_df.show(20)
